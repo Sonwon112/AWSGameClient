5573044ce059b0479a165eeb479c57af06505e1c
Sonwonill <tlatmsdkqk@gmail.com>
2024-11-12 17:01:15 +0900

    :tada: Feat : "첫커밋"
    Scene 구성 완료, 서버 접속 기능 구현 중

---
+++ Assets/Scripts/LoadingText.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LoadingText : MonoBehaviour
+{
+    private TMP_Text LoadText;
+    private string[] loadText = {"연결 중", "연결 중." , "연결 중.." , "연결 중..." };
+    private bool isConnect = false;
+
+    private int index = 0;
+    private float prevTime = 0f;
+    private float currTime = 0f;
+    public float delay = 3f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadText = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isConnect)
+        {
+            currTime += Time.deltaTime;
+
+            if (currTime > prevTime + delay)
+            {
+                if (index + 1 > loadText.Length - 1) index = -1;
+                LoadText.text = loadText[++index];
+
+                prevTime = currTime;
+            }
+        }
+    }
+
+    public void setConnectStat(bool isConnect)
+    {
+        this.isConnect = isConnect;
+    }
+}
